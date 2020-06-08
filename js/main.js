50ed729ea957f523896ef1b34a582ae55e8779dd
Degreet <degreetpro@gmail.com>
2020-06-08 20:10:37 +0300

    added new products to the store

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ if (!localStorage.userClicks) localStorage.userClicks = 0
 if (!localStorage.userCoins) localStorage.userCoins = 0
 if (!localStorage.autoClickers) localStorage.autoClickers = 0
 if (localStorage.autoClickers >= 1) setInterval(() => {for (let i = 0; i < localStorage.autoClickers; i++) coin.click()}, 2000)
+if (localStorage.robot == "true") setInterval(() => {for (let i = 0; i < 25; i++) coin.click()}, 1000)
 
 coin.onclick = () => {
     if (!localStorage.offSound) {
@@ -21,9 +22,11 @@ coin.onclick = () => {
         if (localStorage.minus50score == "true") for (let i = 0; i < 50; i++) localStorage.roundClicksNeed--
         for (let i = 0; i < uppedMoney.innerText; i++) localStorage.userCoins++
         localStorage.userClicks = 0
+        if (localStorage.startSo100) for (let i = 0; i < 100; i++) localStorage.userClicks++
     } else {
         localStorage.userClicks++
         if (localStorage.x2Click == "true") coin.click()
+        if (localStorage.x3Click == "true") for (let i = 0; i < 2; i++) coin.click()
     }
 }
 
@@ -79,6 +82,41 @@ buyOneAutoClicker.onclick = () => {
     }
 }
 
+startSo100.onclick = () => {
+    if (Number(localStorage.userCoins) >= 20 && !localStorage.startSo100) {
+        for (let i = 0; i < 20; i++) localStorage.userCoins--
+        alert("Вы успешно преобрели Старт Со 100!")
+        localStorage.startSo100 = true
+        for (let i = 0; i < 100; i++) localStorage.userClicks++
+    } else {
+        if (Number(localStorage.userCoins) < 20) alert("У вас не достаточно биткоинов!")
+        else if (localStorage.startSo100) alert("Вы уже купили этот товар!")
+    }
+}
+
+x3Click.onclick = () => {
+    if (Number(localStorage.userCoins) >= 40 && !localStorage.x3Click) {
+        for (let i = 0; i < 40; i++) localStorage.userCoins--
+        alert("Вы успешно преобрели x3 клик!")
+        localStorage.x3Click = true
+    } else {
+        if (Number(localStorage.userCoins) < 40) alert("У вас не достаточно биткоинов!")
+        else if (localStorage.x3Click) alert("Вы уже купили этот товар!")
+    }
+}
+
+robot.onclick = () => {
+    if (Number(localStorage.userCoins) >= 80 && !localStorage.robot) {
+        for (let i = 0; i < 80; i++) localStorage.userCoins--
+        alert("Вы успешно преобрели робота!")
+        localStorage.robot = true
+        setInterval(() => {for (let i = 0; i < 25; i++) coin.click()}, 1000)
+    } else {
+        if (Number(localStorage.userCoins) < 80) alert("У вас не достаточно биткоинов!")
+        else if (localStorage.robot) alert("Вы уже купили этот товар!")
+    }
+}
+
 setInterval(() => {
     userClicks.innerText = localStorage.userClicks
     roundClicksNeed.innerText = localStorage.roundClicksNeed
@@ -130,6 +168,51 @@ setInterval(() => {
         if (localStorage.minus50score) {
             minus50score.innerText = 'Куплено за 10 биткоинов'
             minus50score.className = 'btn btn-outline-warning'
+        }
+    }
+
+    if (Number(localStorage.userCoins) >= 20 && !localStorage.startSo100) {
+        startSo100.innerText = 'Купить за 20 биткоинов'
+        startSo100.className = 'btn btn-outline-primary'
+    } else {
+        if (Number(localStorage.userCoins) < 20) {
+            startSo100.innerText = 'Купить за 20 биткоинов'
+            startSo100.className = 'btn btn-outline-danger'
+        }
+        
+        if (localStorage.startSo100) {
+            startSo100.innerText = 'Куплено за 20 биткоинов'
+            startSo100.className = 'btn btn-outline-warning'
+        }
+    }
+
+    if (Number(localStorage.userCoins) >= 40 && !localStorage.x3Click) {
+        x3Click.innerText = 'Купить за 40 биткоинов'
+        x3Click.className = 'btn btn-outline-primary'
+    } else {
+        if (Number(localStorage.userCoins) < 40) {
+            x3Click.innerText = 'Купить за 40 биткоинов'
+            x3Click.className = 'btn btn-outline-danger'
+        }
+        
+        if (localStorage.x3Click) {
+            x3Click.innerText = 'Куплено за 40 биткоинов'
+            x3Click.className = 'btn btn-outline-warning'
+        }
+    }
+
+    if (Number(localStorage.userCoins) >= 80 && !localStorage.robot) {
+        robot.innerText = 'Купить за 80 биткоинов'
+        robot.className = 'btn btn-outline-primary'
+    } else {
+        if (Number(localStorage.userCoins) < 80) {
+            robot.innerText = 'Купить за 80 биткоинов'
+            robot.className = 'btn btn-outline-danger'
+        }
+        
+        if (localStorage.robot) {
+            robot.innerText = 'Куплено за 80 биткоинов'
+            robot.className = 'btn btn-outline-warning'
         }
     }
 }, 50)
